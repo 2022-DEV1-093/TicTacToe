606b6355161a81838c15047855a529c5aeaf8087
2022-DEV1-093 <kumarmohit400@yahoo.com>
2022-11-27 23:36:43 +0530

    Implemented game controller tests

--- src/test/java/com/dev1093/tictactoe/controller/GameControllerTest.java
+++ src/test/java/com/dev1093/tictactoe/controller/GameControllerTest.java
@@ -6,21 +6,30 @@ import com.dev1093.tictactoe.exceptions.PlayerNotFoundException;
 import com.dev1093.tictactoe.services.GameStateService;
 import com.dev1093.tictactoe.services.PlayerService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.dev1093.tictactoe.constants.Constants.*;
 
 @WebMvcTest(value = GameController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -39,51 +48,64 @@ public class GameControllerTest
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeEach
+    public void setUp ()
+    {
+        GameStateService gameStateService1 = new GameStateService();
+        PlayerService playerService1 = new PlayerService();
+        ReflectionTestUtils.setField(playerService, "players", playerService1.getPlayers());
+        ReflectionTestUtils.setField(gameStateService, "board", gameStateService1.getBoard());
+        when(playerService.getPlayers()).thenCallRealMethod();
+        when(playerService.getPlayer(anyString())).thenCallRealMethod();
+        when(gameStateService.getBoard()).thenCallRealMethod();
+        doCallRealMethod().when(gameStateService).resetBoard();
+    }
 
 
     @Test
-    public void testPlayersList () throws Exception
+    public void testGetPlayersList () throws Exception
     {
         mockMvc.perform(get("/players")
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "[{\"id\":\"X\",\"name\":\"Player 1\"},{\"id\":\"O\",\"name\":\"Player 2\"}]"));
     }
 
     @Test
-    public void testPlayer () throws Exception
+    public void testGetPlayer () throws Exception
     {
-        mockMvc.perform(get("/player").param("id","X")
+        mockMvc.perform(get("/player").param("id",X)
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "{\"id\":\"X\",\"name\":\"Player 1\"}"));
     }
 
     @Test
-    public void testState () throws Exception
+    public void testGameBoardState () throws Exception
     {
         mockMvc.perform(get("/state")
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "{\"1\":null,\"2\":null,\"3\":null,\"4\":null,\"5\":null,\"6\":null,\"7\":null,\"8\":null,\"9\":null}"));
     }
 
     @Test
-    public void testReset () throws Exception
+    public void testGameBoardReset () throws Exception
     {
         mockMvc.perform(post("/reset")
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "{\"1\":null,\"2\":null,\"3\":null,\"4\":null,\"5\":null,\"6\":null,\"7\":null,\"8\":null,\"9\":null}"));
     }
 
     @Test
     public void testPlay () throws Exception
     {
+        doCallRealMethod().when(gameStateService).updateState(anyString(), any());
         mockMvc.perform(post("/play").content("{\n"
                     + "\t\"playerId\" : \"X\",\n"
                     + "\t\"position\" : 1\n"
@@ -91,12 +113,24 @@ public class GameControllerTest
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "{\"gameOver\":false,"
+                    + "\"winner\":null,"
+                    + "\"state\":"
+                    + "{\"1\":\"X\","
+                    + "\"2\":null,"
+                    + "\"3\":null,"
+                    + "\"4\":null,"
+                    + "\"5\":null,"
+                    + "\"6\":null,"
+                    + "\"7\":null,"
+                    + "\"8\":null,"
+                    + "\"9\":null}}"));
     }
 
     @Test
-    public void testPlayWithOFirstThrowException () throws Exception
+    public void testXPlaysFirstThrowException () throws Exception
     {
+        doCallRealMethod().when(gameStateService).updateState(anyString(), any());
         mockMvc.perform(post("/play").content("{\n"
                     + "\t\"playerId\" : \"O\",\n"
                     + "\t\"position\" : 5\n"
@@ -104,12 +138,14 @@ public class GameControllerTest
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest())
             .andExpect(response -> assertTrue(response.getResolvedException() instanceof InvalidChoiceException))
-            .andExpect(response -> assertEquals("X always goes first!", response.getResolvedException().getMessage()));
+            .andExpect(response -> assertEquals(X_ALWAYS_PLAYS_FIRST, Objects.requireNonNull(
+                response.getResolvedException()).getMessage()));
     }
 
     @Test
-    public void testPlayOnTakenPositionThrowException () throws Exception
+    public void testGameOverThrowException () throws Exception
     {
+        when(gameStateService.isGameOver()).thenReturn(true);
         mockMvc.perform(post("/play").content("{\n"
                     + "\t\"playerId\" : \"X\",\n"
                     + "\t\"position\" : 5\n"
@@ -117,12 +153,30 @@ public class GameControllerTest
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest())
             .andExpect(response -> assertTrue(response.getResolvedException() instanceof InvalidChoiceException))
-            .andExpect(response -> assertEquals("The position has already been taken", response.getResolvedException().getMessage()));
+            .andExpect(response -> assertEquals(GAME_OVER, Objects.requireNonNull(
+                response.getResolvedException()).getMessage()));
     }
 
     @Test
-    public void testPlayWithInvalidPlayerThrowException () throws Exception
+    public void testTakenPositionThrowException () throws Exception
     {
+        gameStateService.getBoard().put("5", X);
+        doCallRealMethod().when(gameStateService).updateState(anyString(), any());
+        mockMvc.perform(post("/play").content("{\n"
+                    + "\t\"playerId\" : \"X\",\n"
+                    + "\t\"position\" : 5\n"
+                    + "}")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(response -> assertTrue(response.getResolvedException() instanceof InvalidChoiceException))
+            .andExpect(response -> assertEquals(POSITION_TAKEN, Objects.requireNonNull(
+                response.getResolvedException()).getMessage()));
+    }
+
+    @Test
+    public void testInvalidPlayerThrowException () throws Exception
+    {
+        doCallRealMethod().when(gameStateService).updateState(anyString(), any());
         mockMvc.perform(post("/play").content("{\n"
                     + "\t\"playerId\" : \"W\",\n"
                     + "\t\"position\" : 5\n"
@@ -130,12 +184,27 @@ public class GameControllerTest
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest())
             .andExpect(response -> assertTrue(response.getResolvedException() instanceof PlayerNotFoundException))
-            .andExpect(response -> assertEquals("Player W not found", response.getResolvedException().getMessage()));
+            .andExpect(response -> assertEquals("Player W not found", Objects.requireNonNull(
+                response.getResolvedException()).getMessage()));
     }
 
     @Test
-    public void testPlayWinner () throws Exception
+    public void testWinner () throws Exception
     {
+        Map<String, String> boardMap = new HashMap<>();
+        boardMap.put("1", "X");
+        boardMap.put("2", "O");
+        boardMap.put("3", "O");
+        boardMap.put("4", null);
+        boardMap.put("5", "X");
+        boardMap.put("6", null);
+        boardMap.put("7", null);
+        boardMap.put("8", null);
+        boardMap.put("9", null);
+        ReflectionTestUtils.setField(gameStateService, "board", boardMap);
+        when(gameStateService.checkWinner()).thenCallRealMethod();
+        doCallRealMethod().when(gameStateService).endGame(anyBoolean());
+        doCallRealMethod().when(gameStateService).updateState(anyString(), any());
         mockMvc.perform(post("/play").content("{\n"
                     + "\t\"playerId\" : \"X\",\n"
                     + "\t\"position\" : 9\n"
@@ -143,13 +212,29 @@ public class GameControllerTest
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "{\"gameOver\":true,"
+                    + "\"winner\":{\"id\":\"X\",\"name\":\"Player 1\"},"
+                    + "\"state\":{\"1\":\"X\",\"2\":\"O\",\"3\":\"O\",\"4\":null,"
+                    + "\"5\":\"X\",\"6\":null,\"7\":null,\"8\":null,\"9\":\"X\"}}"));
     }
 
     @Test
-    public void testPlayDraw () throws Exception
+    public void testDraw () throws Exception
     {
-
+        Map<String, String> boardMap = new HashMap<>();
+        boardMap.put("1", "X");
+        boardMap.put("2", "X");
+        boardMap.put("3", "O");
+        boardMap.put("4", "O");
+        boardMap.put("5", "O");
+        boardMap.put("6", "X");
+        boardMap.put("7", "X");
+        boardMap.put("8", "O");
+        boardMap.put("9", null);
+        ReflectionTestUtils.setField(gameStateService, "board", boardMap);
+        when(gameStateService.checkWinner()).thenCallRealMethod();
+        doCallRealMethod().when(gameStateService).endGame(anyBoolean());
+        doCallRealMethod().when(gameStateService).updateState(anyString(), any());
         mockMvc.perform(post("/play").content("{\n"
                     + "\t\"playerId\" : \"X\",\n"
                     + "\t\"position\" : 9\n"
@@ -157,6 +242,10 @@ public class GameControllerTest
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(
-                "expectedJsonString"));
+                "{\"gameOver\":true,"
+                    + "\"winner\":{\"id\":\"Draw\","
+                    + "\"name\":\"The game is a draw! No one wins.\"},"
+                    + "\"state\":{\"1\":\"X\",\"2\":\"X\",\"3\":\"O\",\"4\":\"O\","
+                    + "\"5\":\"O\",\"6\":\"X\",\"7\":\"X\",\"8\":\"O\",\"9\":\"X\"}}"));
     }
 }
